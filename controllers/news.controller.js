0ce1aaded703e657abf9667fdc9afb45eec734c3
sushada <32428981+sushada@users.noreply.github.com>
2017-10-07 13:28:38 +0700

    nodejs server+twig

---
+++ controllers/news.controller.js
@@ -0,0 +1,17 @@
+exports.index = function(req,res) {
+	let content = {
+		news:[
+		{
+			id: 1,
+			url: '//image/img-1.jpg',
+			title: 'news 1'
+		},
+		{
+			id: 2,
+			url: '//image/img-1.jpg',
+			title: 'news 2'
+		}
+		]
+	}
+	res.json(content)
+}
